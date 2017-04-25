5b58f0d8dd4a46ff88a42b51ee41bff99ae290f9
Raffaello Martini <rmartini@cisco.com>
2017-04-25 11:18:42 +0100

    added angle_to_grade

--- circuitous.py
+++ circuitous.py
@@ -6,16 +6,57 @@ Build an MVP, then talk to customers.
 "Get out of the building"
 
 YAGNI: Ya ain't gonna need it.
+
+Classes are for SHARED informations
+instances are for UNIQUE informations
+
+bound method        instance.method()
+    regular method call
+    implicitly passes the instance as the first arf
+    
+unbound method      Class.method(instance)
+    calling a method from the class 
+    must explicitly pass the instance as the first arg
+    
+static methond      Class.method()
+    method called from the class
+    but does not require an instance, implicit or explicit 
 '''
+
+from collections import namedtuple
+import math
+
+# namedtuple help readability for versioning
+Version = namedtuple('Version', 'major minor patch')
+
+PI = math.pi
 
 class Circle:
     'an advanced circle analytics toolkit'
 
+    version = Version(0,2,4)
+    # you increase the major when you break compatibility
+    # you increase the minor when you add features
+    # you increase the revision when you patch
+
+
     def __init__(self, radius):
         self.radius = radius
 
+    def __repr__(self):
+        return '{}(radius={!r})'.format(self.__class__.__name__,self.radius)
+        # !r force to use the repr rather than the str
+
     def area(self):
-        return 3.14159 * self.radius ** 2
+        return PI * self.radius ** 2
 
     def circumference(self):
-        return 3.14 * self.radius * 2
+        return PI * self.radius * 2
+
+    @staticmethod
+    # used so you can call Circle.angle_to_grade() without defining an instance first
+    def angle_to_grade(angle):
+        '''
+        Convert an inclinometer reading in degrees to a percent grade  
+        '''
+        return math.tan(math.radians(angle)) * 100.0
--- circuitous_client.py
+++ circuitous_client.py
@@ -3,5 +3,74 @@ Our customers' code.
 '''
 
 from circuitous import Circle
+from random import random, seed
 
-# Customer 0: Our academic friends
+
+def customer_0():
+    '''
+    Customer 0: Our academic friends
+    :return: 
+    '''
+
+    print 'Aproposal to reserach the areas of circles'
+    print 'with Circuitous(tm)', Circle.version
+    n = 5
+    seed(42)
+    #circles = [Circle(random()) for i in xrange(n)]
+    # with parenthesis it becomes a generator
+    circles = (Circle(random()) for i in xrange(n))
+    # areas = [c.area() for c in circles]
+    areas = (c.area() for c in circles)
+    print 'The average aread of', n , 'random circles'
+    print 'seeded with the Answer to Life and Everything'
+    print 'is', sum(areas) / n
+
+
+
+def customer_1():
+    '''Customer 1: Local rubber sheet company
+    company that cuts rounds sheets of rubber
+    '''
+
+    cuts = [0.7, 0.3, 0.5]
+    circles = [Circle(radius) for radius in cuts]
+    for c in circles:
+        print 'A circle with radius', c.radius
+        print 'has an area {:.2f}'.format(c.area())
+        print 'and a perimeter {:.2f}'.format(c.circumference())
+        c.radius *= 1.1
+        print 'and a warm area {:.2f}'.format(c.area())
+
+# customer 2: Regional tyre company
+
+class Tyre(Circle):
+
+    # def __init__(self, radius, pressure):
+    #     Circle.__init__(self, radius)
+    #     self.pressure = pressure
+
+    def circumference(self):
+        'adjusted perimeter for width of tyre'
+        # unbound method call
+        return 1.25 * Circle.circumference(self)
+
+def customer_2():
+    t = Tyre(22)
+    print 'a tyre of radius', 22
+    print 'has an area {:.2f}'.format(t.area())
+    print 'and a circumference {:.2f}'.format(t.circumference())
+
+# Customer 3 - a national trucking company
+
+def customer_3():
+    angle = 7.0
+    print 'a hill with {:.0f} degree incline'.format(angle)
+    print 'is a {:.0f}% grade'.format(Circle.angle_to_grade(7))
+
+
+if __name__ == '__main__':
+    print
+    # customer_0()
+    # customer_1()
+    # customer_2()
+    customer_3()
