b6e37e6ceed5cd785d05043ce23f2694d3e256ac
Raffaello Martini <rmartini@cisco.com>
2017-04-27 17:18:03 +0100

    2017-04-27--17:18:00

--- parallel.py
+++ parallel.py
@@ -5,7 +5,9 @@
 
 import time, random, itertools
 from multiprocessing.pool import ThreadPool
+from multiprocessing.pool import Pool
 from threading import Lock
+# import numpy
 # from collections import Counter
 
 def io_bound(x):
@@ -14,7 +16,9 @@ def io_bound(x):
     return x + 1
 
 def cpu_bound(x):
-    sum(xrange(int(1e8)))
+    sum(xrange(int(1e6)))
+    # numpy.sum(numpy.arange(1e6, dtype='int'))
+    # numpy is quicker because it is compiled in c and is not affected by the multithread lock
     return x + 1
 
 count = 0
@@ -43,13 +47,23 @@ def threaded(func, n, maxthread = 25):
         print result
 
 
+def multiproc(func, n, maxprocs = 25):
+    pool = Pool(min(n, maxprocs))
+    for result in pool.imap_unordered(func, range(n)):
+        print result
+
+
 if __name__ == '__main__':
-    start = time.time()
+    # start = time.time()
     # serial(cpu_bound, 10)
+    # stop = time.time()
+    # print 'serial DURATION {:.2f}'.format(stop - start)
+    start = time.time()
     threaded(cpu_bound, 10)
+    # multiproc(cpu_bound, 10)
     # threaded(cpu_bound, 1000)
     # threaded(tick, 1000)
     stop = time.time()
-    print 'DURATION {:.2f}'.format(stop - start)
+    print 'parallel DURATION {:.2f}'.format(stop - start)
 
     # print count
